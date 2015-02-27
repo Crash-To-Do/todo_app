334297a69b9b4b39f026e6d8a261c2314b2cc23c
Ashley Ellis <anellis12@gmail.com>
2015-02-27 11:02:48 -0500

    Add Create New Todo Integration Test

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,5 +1,4 @@
 class ToDo < ActiveRecord::Base
   validates :title, presence: true
-  validates :due_at, presence: true
   validates :order, presence: true
 end
--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -7,9 +7,6 @@ describe ToDo do
   it "is invalid without a title" do
     expect(FactoryGirl.build(:to_do, title: nil)).to be_invalid
   end
-  it "is invalid without a due_at" do
-    expect(FactoryGirl.build(:to_do, due_at: nil)).to be_invalid
-  end
   it "is invalid without an order number" do
     expect(FactoryGirl.build(:to_do, order: nil)).to be_invalid
   end
