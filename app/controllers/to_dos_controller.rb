b1ac60e2b56d740634f1f368b0c0d6fa04f19d23
Ashley Ellis <anellis12@gmail.com>
2015-02-27 12:53:48 -0500

    Add index page

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,5 +1,7 @@
 class ToDosController < ApplicationController
+
   def index
+    @todos= ToDo.all
   end
 
   def new
--- spec/controllers/to_do_index_spec.rb
+++ spec/controllers/to_do_index_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "to_dos/index", :type => :view do
   it "shows the list of todos in order" do
     FactoryGirl.create_list(:to_do, 3)
-    expect(page).to have_selector('todo', :count => 3)
+    assign(:todos, ToDo.all)
+    render
+    expect(rendered).to have_selector('div.todo', :count => 3)
   end
 end
