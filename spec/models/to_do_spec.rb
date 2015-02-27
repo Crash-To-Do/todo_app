2efd9fe7be51a4b39eb3a2674d2f901f40c937d9
Ashley Ellis <anellis12@gmail.com>
2015-02-27 10:39:41 -0500

    Add first two tests

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,5 +1,17 @@
-require 'rails_helper'
+require_relative '../spec_helper.rb'
 
-RSpec.describe ToDo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ToDo do
+  it "has a valid factory" do
+    expect(FactoryGirl.create(:to_do)).to be_valid
+  end
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:to_do, title: nil)).to be_invalid
+  end
+  # it "is invalid without a name" do
+  #   FactoryGirl.build(:user, name: nil).should_not be_valid
+  # end
+  # it "is invalid without a password" do
+  #   FactoryGirl.build(:user, password: nil).should_not be_valid
+  # end
+  #
 end
