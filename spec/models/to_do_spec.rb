e8d2b7b0444d5d9483c8daa3d9d277da621a42c8
Ashley Ellis <anellis12@gmail.com>
2015-02-27 10:51:14 -0500

    Add new validation tests

--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -2,16 +2,16 @@ require_relative '../spec_helper.rb'
 
 describe ToDo do
   it "has a valid factory" do
-    expect(FactoryGirl.create(:to_do)).to be_valid
+    expect(FactoryGirl.build(:to_do)).to be_valid
   end
   it "is invalid without a title" do
     expect(FactoryGirl.build(:to_do, title: nil)).to be_invalid
   end
-  # it "is invalid without a name" do
-  #   FactoryGirl.build(:user, name: nil).should_not be_valid
-  # end
-  # it "is invalid without a password" do
-  #   FactoryGirl.build(:user, password: nil).should_not be_valid
-  # end
-  #
+  it "is invalid without a due_at" do
+    expect(FactoryGirl.build(:to_do, due_at: nil)).to be_invalid
+  end
+  it "is invalid without an order number" do
+    expect(FactoryGirl.build(:to_do, order: nil)).to be_invalid
+  end
+
 end
