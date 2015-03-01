3cc741fdd3c63294af57f90222970dc1593e7ae5
Chris Foster <fosterchris22@gmail.com>
2015-03-01 11:08:55 -0500

    Change complete to class so all can be checked. #shipit.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,7 +16,7 @@
 //= require turbolinks
 //= require_tree .
 $(function() {
-  $('#to_do_complete').on("click", function() {
+  $('.to-do-complete').on("click", function() {
     var checked;
     var val = $(this).prevAll(".rails-id").val()
     if ($(this).is(':checked')) {
